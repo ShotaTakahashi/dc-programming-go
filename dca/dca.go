c5f4bbbd7fe96d6874873bc468229b65ceb8a48a
ShotaTakahashi <takahashi.s.be@m.titech.ac.jp>
2019-04-21 15:34:57 +0900

    add algorithms

--- dca/dca.go
+++ dca/dca.go
@@ -1,13 +1,80 @@
 package dca
 
-import "math"
+import (
+	"math"
+)
 
-func Dca(xk float64, update func(x float64) float64) float64 {
-	yk := 0.0
-	yk = update(xk)
+var iter = 0
+var alpha = 0.4
+var beta = 0.7
+
+func DCA(xk float64, update func(x float64) float64) (float64, int) {
+	yk := update(xk)
 	if math.Abs(xk-yk) < 1e-10 {
-		return xk
+		return xk, iter
+	}
+	iter++
+	xk = yk
+	return DCA(xk, update)
+}
+
+func BDCA(
+	xk float64,
+	update func(x float64) float64,
+	obj func(x float64) float64,
+) (float64, int) {
+	yk := update(xk)
+	if math.Abs(xk-yk) < 1e-10 {
+		return xk, iter
+	}
+
+	lambda := 1.5
+	dk := yk - xk
+	objVal := obj(yk)
+	for obj(yk+lambda*dk) > objVal-alpha*lambda*dk*dk {
+		lambda *= beta
+	}
+
+	yk = yk + lambda*dk
+	if math.Abs(xk-yk) < 1e-10 {
+		return xk, iter
 	}
 	xk = yk
-	return Dca(xk, update)
+	iter++
+	return BDCA(xk, update, obj)
+}
+
+func BDCAQuadratic(
+	xk float64,
+	update func(x float64) float64,
+	obj func(x float64) float64,
+	grad func(x float64) float64,
+) (float64, int) {
+	yk := update(xk)
+	if math.Abs(xk-yk) < 1e-10 {
+		return xk, iter
+	}
+
+	lambda := 1.5
+	dk := yk - xk
+	objVal := obj(yk)
+	objLambda := obj(yk + lambda*dk)
+	gradDk := grad(yk) * dk
+	optLambda := -gradDk * lambda * lambda / (2 * (objLambda - objVal - gradDk*lambda))
+
+	if optLambda > 0 && obj(yk+optLambda*dk) < objLambda {
+		lambda = math.Min(1.5, optLambda)
+	}
+
+	for obj(yk+lambda*dk) > objVal-alpha*lambda*dk*dk {
+		lambda *= beta
+	}
+
+	yk = yk + lambda*dk
+	if math.Abs(xk-yk) < 1e-10 {
+		return xk, iter
+	}
+	xk = yk
+	iter++
+	return BDCAQuadratic(xk, update, obj, grad)
 }
--- main.go
+++ main.go
@@ -9,12 +9,28 @@ import (
 
 func main() {
 	var x0 = 27.0 / 125.0
+
 	t0 := time.Now()
-	solution := dca.Dca(
-		x0,
-		func(x float64) float64 {
-			return math.Pow(x, 1.0/3.0)
-		},
-	)
-	fmt.Println(time.Since(t0), solution)
+	opt, iter := dca.DCA(x0, update)
+	fmt.Println(time.Since(t0), opt, iter)
+
+	t0 = time.Now()
+	opt, iter = dca.BDCA(x0, update, obj)
+	fmt.Println(time.Since(t0), opt, iter)
+
+	t0 = time.Now()
+	opt, iter = dca.BDCAQuadratic(x0, update, obj, grad)
+	fmt.Println(time.Since(t0), opt, iter)
+}
+
+func update(x float64) float64 {
+	return math.Pow(x, 1.0/3.0)
+}
+
+func obj(x float64) float64 {
+	return math.Pow(x, 4)/4.0 - math.Pow(x, 2)/2.0
+}
+
+func grad(x float64) float64 {
+	return math.Pow(x, 3) - x
 }
